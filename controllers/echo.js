a9e30a1e0dfa99570860f1eb2d507c53e13c8d5f
Kunanon <pollamacks@hotmail.com>
2021-05-19 00:20:10 +0700

    update echo route

--- controllers/echo.js
+++ controllers/echo.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 
 router.get('/echo_get', (req, res) => {
-  res.json('Echo from router...');
+  res.json({ message: 'Echo from router...' });
 });
 
 router.get('/echo_qs', (req, res) => {
   const query = req.query;
-  res.json(query);
+  res.json({ query });
 });
 
 router.get('/echo_param/:message', (req, res) => {
   const message = req.params.message;
-  res.json(message);
+  res.json({ params: message });
 });
 
 router.post('/echo_post', (req, res) => {
   // req.body parse from body-parser middleware
   const body = req.body;
-  res.json(body);
+  res.json({ body });
 });
 
 module.exports = router;
