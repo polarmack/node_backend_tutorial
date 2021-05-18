e891ff0564981d8009765cf3c0de94acbbd65eed
Kunanon <pollamacks@hotmail.com>
2021-05-18 17:49:30 +0700

    update todo route

--- controllers/todo.js
+++ controllers/todo.js
@@ -3,12 +3,59 @@ const User = require('../models/Todo');
 
 router.get('/no_auth/', async (req, res) => {
   const todos = await User.find();
+
   res.json({ succes: true, count: todos.length, data: todos });
+});
+
+router.get('/no_auth/:id', async (req, res) => {
+  const todo = await User.findById(req.params.id);
+
+  if (!todo) {
+    return res.status(400).json({
+      success: false,
+      error: `Todo not found with id ${req.params.id}`,
+      data: {},
+    });
+  }
+
+  res.json({ succes: true, data: todo });
 });
 
 router.post('/no_auth/', async (req, res) => {
   const todo = await User.create(req.body);
+
   res.json({ succes: true, data: todo });
+});
+
+router.put('/no_auth/:id', async (req, res) => {
+  const updateTodo = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!updateTodo) {
+    return res.status(400).json({
+      success: false,
+      error: `Todo not found with id ${req.params.id}`,
+      data: {},
+    });
+  }
+
+  res.json({ succes: true, data: updateTodo });
+});
+
+router.delete('/no_auth/:id', async (req, res) => {
+  const todo = await User.findByIdAndDelete(req.params.id);
+
+  if (!todo) {
+    return res.status(400).json({
+      success: false,
+      error: `Todo not found with id ${req.params.id}`,
+      data: {},
+    });
+  }
+
+  res.json({ succes: true, data: {} });
 });
 
 module.exports = router;
--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -16,5 +16,5 @@ module.exports = (err, req, res, next) => {
 
   res
     .status(err.status || 500)
-    .json({ success: false, error: err.message, data: {} });
+    .json({ success: false, error: err.message || 'Unhandle error', data: {} });
 };
